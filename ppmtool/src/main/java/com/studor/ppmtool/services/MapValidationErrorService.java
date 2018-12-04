7b1fe8562de459b5c05f60051ef5bb8c570e268b
Sylwinn Tudor <swtudor@gmail.com>
2018-12-04 12:58:03 -0600

    Added better error handling

---
+++ ppmtool/src/main/java/com/studor/ppmtool/services/MapValidationErrorService.java
@@ -0,0 +1,24 @@
+package com.studor.ppmtool.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+         public ResponseEntity<?>MapValidationService(BindingResult br) {
+            if (br.hasErrors()) {
+                Map<String, String> errorMap = new HashMap<>();
+                for (FieldError err : br.getFieldErrors()) {
+                    errorMap.put(err.getField(), err.getDefaultMessage());
+                }
+                return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+            }
+            return null;
+        }
+}
--- ppmtool/src/main/java/com/studor/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/studor/ppmtool/web/ProjectController.java
@@ -1,14 +1,18 @@
 package com.studor.ppmtool.web;
 
 import com.studor.ppmtool.domain.Project;
+import com.studor.ppmtool.services.MapValidationErrorService;
 import com.studor.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/project")
@@ -17,8 +21,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult br){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(br);
+        if(errorMap != null){return errorMap;}
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
