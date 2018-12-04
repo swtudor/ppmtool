c32bb4ba7aa637a846bfb4fd47f2c350f9d19a92
Sylwinn Tudor <swtudor@gmail.com>
2018-12-04 15:47:31 -0600

    Adding GET, GET All and DELETE methods.

--- ppmtool/src/main/java/com/studor/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/com/studor/ppmtool/repositories/ProjectRepository.java
@@ -1,9 +1,14 @@
 package com.studor.ppmtool.repositories;
 
 import com.studor.ppmtool.domain.Project;
+import com.studor.ppmtool.exceptions.ProjectIdException;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
     @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Iterable<Project> findAll();
+
+    Project findByProjectIdentifier(String projectId);
 }
